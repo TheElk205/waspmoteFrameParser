bb92fc3f4dbe5f55f1afcca75817960ce1f4b80e
Ferdinand <fkoeppen@edu.auu.at>
2018-01-22 15:51:17 +0100

    Added stup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="Waspmote frame parser",
+    version="0.1",
+    author="Ferdinand Koeppen",
+    autohr_email="fkoeppen@edu.aau.at",
+    description="Frame parser for waspmote frames",
+    url="https://github.com/TheElk205/waspmote-frame-parser",
+    license="MIT"
+)
